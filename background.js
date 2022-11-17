8e4e90bcb8bd0091fca17e71d39f41a2b3cd87e4
Namefix <41016273+Namefix@users.noreply.github.com>
2022-11-17 20:20:35 +0300

    Separated seen status for stories and chat messages

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
-function allowSeen() {
+function updateSeen(story, chat) { // TRUE = BLOCK / FALSE = ALLOW
     chrome.declarativeNetRequest.updateDynamicRules({
         removeRuleIds: [1],
         addRules: [
             {
                 id: 1,
                 condition: {
-                    urlFilter: `https://i.instagram.com/api/v1/stories/reel/seen`,
+                    urlFilter: `https://www.instagram.com/api/v1/stories/reel/seen`,
                     resourceTypes: ['xmlhttprequest'],
                 },
-                action: { type: 'allow' },
+                action: { type: story?"block":"allow" },
             }
         ],
     });
@@ -18,52 +18,24 @@ function allowSeen() {
             {
                 id: 2,
                 condition: {
-                    urlFilter: `https://i.instagram.com/api/v1/direct_v2/threads/*/items/*/seen/`,
+                    urlFilter: `https://www.instagram.com/api/v1/direct_v2/threads/*/items/*/seen/`,
                     resourceTypes: ['xmlhttprequest'],
                 },
-                action: { type: 'allow' },
-            }
-        ],
-      });
-}
-
-function denySeen() {
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [1],
-        addRules: [
-            {
-                id: 1,
-                condition: {
-                    urlFilter: `https://i.instagram.com/api/v1/stories/reel/seen`,
-                    resourceTypes: ['xmlhttprequest'],
-                },
-                action: { type: 'block' },
+                action: { type: chat?"block":"allow" },
             }
         ],
     });
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [2],
-        addRules: [
-            {
-                id: 2,
-                condition: {
-                    urlFilter: `https://i.instagram.com/api/v1/direct_v2/threads/*/items/*/seen/`,
-                    resourceTypes: ['xmlhttprequest'],
-                },
-                action: { type: 'block' },
-            }
-        ],
-      });
+    console.log(story, story?"block":"allow", chat, chat?"block":"allow")
 }
 
 chrome.runtime.onInstalled.addListener(async () => {
-    chrome.storage.sync.set({ status: true });
+    chrome.storage.sync.set({ status: {story:true, chat:true} });
 
     let url = chrome.runtime.getURL("index.html");
   
     chrome.tabs.create({ url });
 
-    denySeen();
+    updateSeen(true, true);
 });
 
 chrome.windows.onCreated.addListener(function() {
@@ -71,29 +43,24 @@ chrome.windows.onCreated.addListener(function() {
     reciever.then((storage) => {
         let status = storage.status;
         
-        if(status) {
-            denySeen();
-            chrome.action.setIcon({path: "icons/Unseen128.png"});
-        } else {
-            allowSeen();
-            chrome.action.setIcon({path: "icons/Seen128.png"});
-        }
+        console.log(status)
+        updateSeen(status.story, status.chat);
+        if(status.story || status.chat) chrome.action.setIcon({path: "icons/Unseen128.png"});
+        else chrome.action.setIcon({path: "icons/Seen128.png"});
     });
 })
 
 
 chrome.storage.onChanged.addListener((changes, areaname) => {
+    console.log("hey", changes)
     if(!changes.status) return;
     let reciever = chrome.storage.sync.get("status");
     reciever.then((storage) => {
         let status = storage.status;
-        
-        if(status) {
-            denySeen();
-            chrome.action.setIcon({path: "icons/Unseen128.png"});
-        } else {
-            allowSeen();
-            chrome.action.setIcon({path: "icons/Seen128.png"});
-        }
+
+        console.log(status)
+        updateSeen(status.story, status.chat);
+        if(status.story || status.chat) chrome.action.setIcon({path: "icons/Unseen128.png"});
+        else chrome.action.setIcon({path: "icons/Seen128.png"});
     });
 });
--- popup.js
+++ popup.js
@@ -1,40 +1,61 @@
-let button = document.querySelector(".onoff");
+let storyswitch = document.querySelector(".storyswitch input");
+let chatswitch = document.querySelector(".chatswitch input");
 let seentext = document.querySelector(".status");
 
-let reciever = chrome.storage.sync.get("status");
-reciever.then((storage) => {
-    let status = storage.status;
-    console.log(status)
-    if(status) {
-        button.classList.add("active");
-        button.innerText = "ON";
-        seentext.classList.add("active"); 
-        seentext.innerText = "Unseen";
-    } else {
-        button.classList.remove("active");
-        button.innerText = "OFF";
-        button.classList.remove("active");
-        seentext.innerText = "Seen";
-    }
-});
-
-
-button.addEventListener("click", () => {
+function update() {
     let reciever = chrome.storage.sync.get("status");
     reciever.then((storage) => {
         let status = storage.status;
-        if(status) {
-            chrome.storage.sync.set({ status: false });
-            button.classList.remove("active");
-            button.innerText = "OFF";
-            seentext.classList.remove("active");
-            seentext.innerText = "Seen";
+        console.log(status)
+        let updated = false;
+        if(status.story) {
+            storyswitch.checked = true;
+            updated=true;
         } else {
-            chrome.storage.sync.set({ status: true });
-            button.classList.add("active");
-            button.innerText = "ON";
-            seentext.classList.add("active");
+            storyswitch.checked = false;
+        }
+        if(status.chat) {
+            chatswitch.checked = true;
+            updated=true;
+        } else {
+            chatswitch.checked = false;
+        }
+
+        if(updated) {
+            seentext.classList.add("active"); 
             seentext.innerText = "Unseen";
+        } else {
+            seentext.innerText = "Seen";
+            seentext.classList.remove("active"); 
+        }
+    });
+}
+update();
+
+
+storyswitch.addEventListener("click", () => {
+    let reciever = chrome.storage.sync.get("status");
+    reciever.then((storage) => {
+        let status = storage.status;
+        if(status.story) {
+            chrome.storage.sync.set({ status: {story:false,chat:status.chat} });
+            update();
+        } else {
+            chrome.storage.sync.set({ status: {story:true,chat:status.chat} });
+            update();
+        }
+    })
+});
+chatswitch.addEventListener("click", () => {
+    let reciever = chrome.storage.sync.get("status");
+    reciever.then((storage) => {
+        let status = storage.status;
+        if(status.chat) {
+            chrome.storage.sync.set({ status: {story:status.story,chat:false} });
+            update();
+        } else {
+            chrome.storage.sync.set({ status: {story:status.story,chat:true} });
+            update();
         }
     })
 });
